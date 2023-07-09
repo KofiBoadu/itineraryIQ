e2e5676dd7bdbafb99ca2e09f1ec8eef1fd82375
Daniel Kofi Boadu <mrboadu3@gmail.com>
2023-07-08 19:06:18 -0500

    created a prompt function to concatenate the users inputs and pass it as a prompt to our AI function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template,request
 from models.itineraryAI import itineraryAI
 
 app=Flask(__name__)
@@ -6,4 +6,13 @@ app=Flask(__name__)
 
 prompt= "create a 10 days itinerary for Ghana .with a budget of 30000, my interest is culture and safari "
 
-print(itineraryAI(prompt))
+@app.route("/",methods=["GET"])
+def home_page():
+    if request.method=="GET":
+        return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
+    
+
